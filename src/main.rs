2fc9c4c51a927f4c9e988f04e5b7b59f18e07065
Fabian Eriksson <fabian.eriksson@r2m.se>
2020-12-10 17:08:39 +0100

    Added Post-message functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 
-use reqwest::blocking::Client;
+use reqwest::blocking::{Client, Response};
 use std::collections::HashMap;
 use serde::Deserialize;
 
@@ -33,13 +33,13 @@ fn get_channels(client: &Client) -> Result<Vec<Channel>, Box<dyn Error>> {
     loop {
         let mut body: ChannelResponse = client.post("https://slack.com/api/conversations.list")
             .form(&params)
-            .send()
-            ?.json()?;
+            .send()?
+            .json()?;
 
         let cursor = body.response_metadata.next_cursor;
         channels.append(&mut body.channels);
         if cursor.is_empty() {
-            break
+            break;
         }
         params.insert("cursor", cursor);
     }
@@ -47,8 +47,22 @@ fn get_channels(client: &Client) -> Result<Vec<Channel>, Box<dyn Error>> {
     Ok(channels)
 }
 
-fn post_message(client: &Client, channel: &Channel, message: &str) {
+fn post_message(client: &Client, channel: &Channel, message: &str) -> Result<(), Box<dyn Error>> {
+    let mut params = HashMap::new();
 
+    params.insert("token", TOKEN);
+    params.insert("channel", channel.id.as_str());
+    params.insert("text", message);
+
+    let resp = client.post("https://slack.com/api/chat.postMessage")
+        .form(&params)
+        .send()?;
+
+    if resp.status().is_success() {
+        Ok(())
+    } else {
+        Err(resp.status().as_str().into())
+    }
 }
 
 fn main() {
@@ -58,6 +72,9 @@ fn main() {
 
     for channel in &channels {
         println!("{:?}", channel);
+        if channel.name == "joel-bot" {
+            post_message(&client, channel, "Hej allihopa, det är jag som är jo3ll-bot");
+        }
     }
 
     println!("{:?}", channels.len());
