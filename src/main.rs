ea155940b403836470d3775d36db448bd6de3641
Petter Salminen <kemikaze+github@gmail.com>
2024-09-29 22:02:17 +0200

    Fix out-of-range error in random generation
    The previous code could produce an index that was out of bounds. Changing the range to `0..calculations.len()` ensures it stays within valid limits. This prevents potential runtime panics.

--- src/main.rs
+++ src/main.rs
@@ -119,7 +119,7 @@ fn time_report(request: Form<SlackSlashMessage>) -> String {
 
                     let mut rng = thread_rng();
                     for _ in 0..2 {
-                        let pos = rng.gen_range(0..calculations.len() - 1);
+                        let pos = rng.gen_range(0..calculations.len());
 
                         map.insert("text", format!("... {}", calculations[pos]));
 
