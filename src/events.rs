482f80de9965fc2a38ce7196fdf15bd154049874
Petter Salminen <kemikaze+github@gmail.com>
2024-10-04 07:55:10 +0200

    WIP in progress, no compile or works.
    Trying to make whole app async to work with rocket async...

--- src/events.rs
+++ src/events.rs
@@ -1,6 +1,11 @@
-use crate::client::SlackClient;
+use crate::client::{SlackClient, SlackClientTrait};
+use crate::last_day::{get_last_workday, is_last_workday};
+use chrono::Utc;
 use serde::Deserialize;
 use std::sync::atomic::{AtomicPtr, Ordering};
+use crate::config::Configuration;
+
+use rocket::{config, Config};
 
 #[derive(Deserialize)]
 #[serde(tag = "type")]
@@ -21,6 +26,7 @@ pub struct ChallengeRequest {
 #[derive(Deserialize)]
 pub struct EventRequest {
     // TODO: Add to use when we get request
+    #[allow(dead_code)]
     token: String,
     pub event: Event,
 }
@@ -39,46 +45,87 @@ pub enum Event {
     AppMentionEvent(AppMentionEvent),
 }
 
-pub struct SlackEvents {
+pub struct SlackState {
     token: AtomicPtr<String>,
     slack_client: SlackClient,
-    app_mention_event_handler: fn(&SlackClient, AppMentionEvent) -> String,
 }
 
-impl SlackEvents {
-    pub fn new() -> SlackEvents {
-        SlackEvents {
-            app_mention_event_handler: |_s, _e| String::new(), // Default func
+impl SlackState {
+    pub fn new() -> Self
+    {
+        SlackState {
             slack_client: SlackClient::new().unwrap(),
-            token: AtomicPtr::new(&mut String::new()),
+            token: AtomicPtr::new(Box::into_raw(Box::new(String::new()))), // Fixing memory management
         }
     }
 
-    pub fn set_mention_event_handler(&mut self, func: fn(&SlackClient, AppMentionEvent) -> String) {
-        self.app_mention_event_handler = func
-    }
-
-    fn handle_challenge_request(&self, request: ChallengeRequest) -> String {
+    async fn handle_challenge_request(&self, request: ChallengeRequest) -> String {
         self.token
             .store(&mut String::from(request.token), Ordering::Relaxed);
-
         request.challenge
     }
 
-    fn verify_event_then_call(&self, req: EventRequest) -> String {
+    async fn verify_event_then_call(&self, req: EventRequest) -> String {
         // TODO: Verify the token then allow the request.
 
         match req.event {
             Event::AppMentionEvent(event) => {
-                (self.app_mention_event_handler)(&self.slack_client, event)
+                SlackState::handle_mention_event(&self.slack_client, event).await
             }
         }
     }
 
-    pub fn handle_request(&self, request: SlackRequest) -> String {
+    pub async fn handle_request(&self, request: SlackRequest) -> String {
         match request {
-            SlackRequest::Challenge(request) => self.handle_challenge_request(request),
-            SlackRequest::Event(request) => self.verify_event_then_call(request),
+            SlackRequest::Challenge(request) => self.handle_challenge_request(request).await,
+            SlackRequest::Event(request) => self.verify_event_then_call(request).await,
         }
     }
+
+    async fn handle_mention_event(
+        client: &impl SlackClientTrait,
+        event: AppMentionEvent,
+    ) -> String {
+        let config = Configuration::read().expect("couldn't read configuration when mentioned");
+        let mut splits: Vec<&str> = event.text.split(" ").collect();
+        splits.drain(0..1);
+
+        let message: String = if splits.len() > 0 {
+            match splits[0] {
+                "tid" => {
+                    let today = Utc::now().naive_utc().date();
+                    match get_last_workday(&today).await {
+                        Ok(last_workday) => {
+                            if last_workday == today {
+                                format!("Okej, jag har kikat i kalendern och det är först *{}* som du behöver tidrapportera!\n\n... vänta\n... beräknar\n... det är ju idag!", last_workday)
+                            } else {
+                                format!("Okej, jag har kikat i kalendern och det är först *{}* som du behöver tidrapportera!", last_workday)
+                            }
+                        }
+                        Err(error) => {
+                            println!("{}", error);
+                            String::from("Herregud någonting gick skitfel! Jag kanske behöver uppdatera min firmware :joel:. Kan någon snälla kolla loggen i Azure?")
+                        }
+                    }
+                }
+                "pricing" => {
+                    String::from("För den nätta kostnaden av 114,805 kr per månad eller 15,8 öre per timme kan du hosta din egen joel-bot! :joel:")
+                }
+                "skribenter" => {
+                    config.get_authors()
+                }
+                _command => {
+                    format!("Är du skön eller <@{}>? Tror du att _jag_ vet något om *{}*? :joel:", event.user, splits.join(" "))
+                }
+            }
+        } else {
+            config.get_introduction()
+        };
+        client
+            .post_message(&event.channel, &message).await
+            .unwrap_or_else(|error| println!("{}", error));
+
+        String::new()
+    }
+
 }
--- src/last_day.rs
+++ src/last_day.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 
-use chrono::{DateTime, Datelike, NaiveDate, Utc};
-use reqwest::blocking::Client;
+use chrono::{Datelike, NaiveDate};
+use reqwest::Client;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -23,11 +23,12 @@ struct SholidayFaboulDay {
     work_free_day: String,
 }
 
-pub fn is_last_workday(date: &DateTime<Utc>) -> Result<bool, Box<dyn Error>> {
-    Ok(get_last_workday(date)? == date.naive_utc().date())
+
+pub async fn is_last_workday(date: &NaiveDate) -> Result<bool, Box<dyn Error>> {
+    Ok(get_last_workday(date).await? == *date)
 }
 
-pub fn get_last_workday(date: &DateTime<Utc>) -> Result<NaiveDate, Box<dyn Error>> {
+pub async fn get_last_workday(date: &NaiveDate) -> Result<NaiveDate, Box<dyn Error>> {
     let client = Client::new();
     let url = format!(
         "https://sholiday.faboul.se/dagar/v2.1/{}/{}",
@@ -35,7 +36,7 @@ pub fn get_last_workday(date: &DateTime<Utc>) -> Result<NaiveDate, Box<dyn Error
         date.month()
     );
 
-    let response: SholidayFaboulResponse = client.get(url.as_str()).send()?.json()?;
+    let response: SholidayFaboulResponse = client.get(url.as_str()).send().await?.json().await?;
 
     let last_work_day = response
         .days
@@ -46,36 +47,47 @@ pub fn get_last_workday(date: &DateTime<Utc>) -> Result<NaiveDate, Box<dyn Error
     let sholiday_day_date =
         NaiveDate::parse_from_str(last_work_day.date.clone().as_str(), "%Y-%m-%d")?;
 
-    return Ok(sholiday_day_date);
+    Ok(sholiday_day_date)
 }
 
 #[cfg(test)]
 mod test {
-    use chrono::{TimeZone, Utc};
+    use chrono::{NaiveDate, TimeZone, Utc};
+    use tokio;
 
-    use crate::last_day::is_last_workday;
+    use super::{is_last_workday, get_last_workday};
 
-    #[test]
-    fn is_2020_10_31_last_work_day() {
-        let date = Utc.with_ymd_and_hms(2020, 10, 31, 0, 0, 0).unwrap();
-        let is_last = is_last_workday(&date).expect("failed");
+    #[tokio::test]
+    async fn is_2020_10_31_last_work_day() {
+        let date = NaiveDate::from_ymd_opt(2020, 10, 31).unwrap();
+        let is_last = is_last_workday(&date).await.expect("failed");
 
         assert!(!is_last)
     }
 
-    #[test]
-    fn is_2020_10_30_last_work_day() {
-        let date = Utc.with_ymd_and_hms(2020, 10, 30, 0, 0, 0).unwrap();
-        let is_last = is_last_workday(&date).expect("failed");
+    #[tokio::test]
+    async fn is_2020_10_30_last_work_day() {
+        let date = NaiveDate::from_ymd_opt(2020, 10, 30).unwrap();
+        let is_last = is_last_workday(&date).await.expect("failed");
 
         assert!(is_last)
     }
 
-    #[test]
-    fn is_2020_10_29_last_work_day() {
-        let date = Utc.with_ymd_and_hms(2020, 10, 29, 0, 0, 0).unwrap();
-        let is_last = is_last_workday(&date).expect("failed");
+    #[tokio::test]
+    async fn is_2020_10_29_last_work_day() {
+        let date = NaiveDate::from_ymd_opt(2020, 10, 29).unwrap();
+        let is_last = is_last_workday(&date).await.expect("failed");
 
         assert!(!is_last)
+    }
+
+    #[tokio::test]
+    async fn test_get_last_workday() {
+        let date = NaiveDate::from_ymd_opt(2020, 10, 31).unwrap();
+        let last_workday = get_last_workday(&date).await.expect("failed");
+
+        // 2020-10-30 was a Friday, and therefore the last workday of October 2020
+        let expected_last_workday = Utc.with_ymd_and_hms(2020, 10, 30, 0, 0, 0).unwrap().date_naive();
+        assert_eq!(last_workday, expected_last_workday);
     }
 }
